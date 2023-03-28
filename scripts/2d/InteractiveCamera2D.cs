9f433b612c1679b30e5fe7c9dcfc1690de8bca9b
AstralSheep <109028693+Astral-Sheep@users.noreply.github.com>
2023-03-28 11:55:11 +0200

    feat: modify 2d camera movement

--- scripts/2d/InteractiveCamera2D.cs
+++ scripts/2d/InteractiveCamera2D.cs
@@ -4,22 +4,32 @@ namespace Com.Astral.WFC._2D
 {
 	public partial class InteractiveCamera2D : Camera2D
 	{
-		protected const string UP_ACTION = "forward";
-		protected const string DOWN_ACTION = "back";
-		protected const string LEFT_ACTION = "left";
-		protected const string RIGHT_ACTION = "right";
-
 		[Export] protected Key resetKey = Key.Backspace;
-		[Export] protected float movementSpeed = 100f;
+		[Export] protected float zoomSpeed = 1f;
 
 		public override void _Process(double pDelta)
 		{
-			Vector2 lVelocity = new Vector2(
-				Input.GetActionStrength(RIGHT_ACTION) - Input.GetActionStrength(LEFT_ACTION),
-				Input.GetActionStrength(DOWN_ACTION) - Input.GetActionStrength(UP_ACTION)
-			).Normalized() * movementSpeed;
+			if (Input.IsMouseButtonPressed(MouseButton.Right))
+			{
+				Position -= Input.GetLastMouseVelocity() / Zoom * (float)pDelta;
+			}
+		}
 
-			Position += lVelocity * (float)pDelta;
+		public override void _UnhandledInput(InputEvent pEvent)
+		{
+			if (pEvent is InputEventMouseButton pMouseEvent)
+			{
+				if (pMouseEvent.ButtonIndex == MouseButton.WheelDown && Zoom.X > zoomSpeed)
+				{
+					Zoom -= Vector2.One * zoomSpeed * Zoom.X;
+				}
+				else if (pMouseEvent.ButtonIndex == MouseButton.WheelUp)
+				{
+					Zoom += Vector2.One * zoomSpeed * Zoom.X;
+				}
+			}
+
+			base._UnhandledInput(pEvent);
 		}
 
 		public override void _UnhandledKeyInput(InputEvent pEvent)
